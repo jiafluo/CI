288f57b53992a4ba43b52abdddd1dd6418428f5e
jiafluo <jiafluo@yeah.net>
2022-04-08 23:24:33 +0800

    add ci/cd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + CI/CD" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + CI/CD11" />
   </div>
 </template>
 
