dad00987c5002ac310c8ebd6502f6d425ad6027b
jiafluo <jiafluo@yeah.net>
2022-04-08 22:47:05 +0800

    add baota

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    44
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + Baota" />
   </div>
 </template>
 
